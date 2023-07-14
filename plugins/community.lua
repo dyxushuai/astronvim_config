6ad72f38ed37388e45c9767a2b66d6fb8c5c2c9d
john xu <dyxushuai@gmail.com>
2023-07-14 17:13:05 +0800

    first step

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,18 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.color.vim-highlighter" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
