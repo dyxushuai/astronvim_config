6695119e7a8d30fe6685322582e66eab75573350
john xu <dyxushuai@gmail.com>
2023-07-21 19:00:56 +0800

    Update

--- plugins/community.lua
+++ plugins/community.lua
@@ -14,6 +14,7 @@ return {
   { import = "astrocommunity.completion.cmp-cmdline" },
   { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
   { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.register.nvim-neoclip-lua" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   -- { import = "astrocommunity.colorscheme.catppuccin" },
